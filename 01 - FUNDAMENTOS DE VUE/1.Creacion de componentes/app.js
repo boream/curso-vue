56708dd5a5095d497340dc781189bbc56dbc33e0
Alejandro Garrido <alejandro.garrido@boream.com>
2018-11-30 11:28:20 +0100

    Base para el curso

--- 01 - FUNDAMENTOS DE VUE/1.Creacion de componentes/app.js
+++ 01 - FUNDAMENTOS DE VUE/1.Creacion de componentes/app.js
@@ -5,9 +5,9 @@ Vue.component('my-first-component', {
       <h2>{{ title }}</h2>  
     </div>
   `,
-})
+});
 // Importante - La instancia de Vue debe estar declarada debajo de la declaración del componente.
 // Sino el componente no sera reconocido.
 new Vue({
     el: '#app'
-})
+});
